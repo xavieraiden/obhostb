bae083405469ab18e6f2b77d95ec74e343556336
xavieraiden <xavieraidenhooke@gmail.com>
2023-09-03 03:56:17 +1000

    Quartz sync: Sep 3, 2023, 3:56 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,13 +3,13 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Wiki Thing",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "xavieraiden.github.io/obhostb/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
