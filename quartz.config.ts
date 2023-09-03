bdb9c592447ed303e5390353efcb1c9b097a1e23
xavieraiden <xavieraidenhooke@gmail.com>
2023-09-03 11:28:08 +1000

    Quartz sync: Sep 3, 2023, 11:28 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "xavieraiden.github.io/obhostb/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
